52a2e1ff64a315d0594134e5cc64b66565689739
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-23 03:58:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,4 +16,20 @@ def author=(author)
    @author
  end
 
+ def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
 end
